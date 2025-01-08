70a7151e3865c9a9d2b02e82b060bce98e59c2cc
mikha <mikha@DESKTOP-A55NKDU>
2025-01-08 20:33:42 +0400

    Добавьте файлы проекта.

---
+++ TaskManagementApp/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagementApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } // имя пользователя
+        public int Age { get; set; } // возраст пользователя
+
+    }
+}
