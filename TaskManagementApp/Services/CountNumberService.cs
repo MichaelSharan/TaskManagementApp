7d876a5d763060d8e1428b46924aaf9e770a36e7
Anastasia Kuzmina <kuzminaanastasya2000@gmail.com>
2025-01-29 17:55:48 +0400

    feat: made service faster

--- TaskManagementApp/Services/CountNumberService.cs
+++ TaskManagementApp/Services/CountNumberService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public class CountNumberService
 {
@@ -10,36 +11,29 @@ public class CountNumberService
             return "NO";
         }
 
-        // Преобразуем строку в массив символов и сортируем по убыванию
-        char[] digits = input.OrderByDescending(c => c).ToArray();
+        List<char> digits = input.ToList();
+        digits.Sort((a, b) => b.CompareTo(a)); // Быстрая сортировка по убыванию
 
-        // Ищем первую нечётную цифру с конца массива
         int oddIndex = -1;
-        for (int i = digits.Length - 1; i >= 0; i--)
+        for (int i = digits.Count - 1; i >= 0; i--)
         {
-            if ((digits[i] - '0') % 2 == 1) // Преобразуем в число и проверяем на нечётность
+            if ((digits[i] - '0') % 2 == 1)
             {
                 oddIndex = i;
                 break;
             }
         }
 
-        // Если нечётной цифры нет, возвращаем "NO"
         if (oddIndex == -1)
         {
             return "NO";
         }
 
-        // Перемещаем нечётную цифру в конец массива
         char oddDigit = digits[oddIndex];
-        for (int i = oddIndex; i < digits.Length - 1; i++)
-        {
-            digits[i] = digits[i + 1];
-        }
-        digits[digits.Length - 1] = oddDigit;
+        digits.RemoveAt(oddIndex); // Быстрее, чем сдвиг элементов
 
-        // Убираем ведущие нули
-        string result = new string(digits).TrimStart('0');
+        string result = new string(digits.ToArray()) + oddDigit;
+        result = result.TrimStart('0');
 
         return string.IsNullOrEmpty(result) ? "NO" : result;
     }
